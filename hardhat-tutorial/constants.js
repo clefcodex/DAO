54d3042d047370d6eae6f5a65bdfa0f1ca9495e0
clefcodex <iwewale1@gmail.com>
2022-06-01 17:55:53 +0100

    Initial Commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x6537af084909fce3Ce8fce0451b5131d26BF1242";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
